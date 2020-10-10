dbe111cde9611e1cea4fa83ae5584be8c343fb8f
jerjunkel <jerjunkel@gmail.com>
2020-10-10 18:28:16 -0400

    add morgan logger middleware

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
+//////////////////////////////////////
+////////////MIDDLEWARE////////////////
+//////////////////////////////////////
+
+// Add morgan logger middleware
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("HELLO");
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
