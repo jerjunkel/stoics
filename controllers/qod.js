d1fa71c2a51dba825e50959108b5f06fbb7ba1a3
jerjunkel <jerjunkel@gmail.com>
2020-10-18 22:37:32 -0400

    respond QOD with author, quote and day

--- controllers/qod.js
+++ controllers/qod.js
@@ -1,9 +1,16 @@
 const Quote = require("../models/Quote");
+const Author = require("../models/Author");
 
 // @desc   Get the day quote
 // @route  GET /
 // @access Public
 exports.getQOD = async (req, res) => {
-  const quote = await Quote.find({});
-  res.status(200).json(quote);
+  try {
+    const { quote, day, author } = await Quote.findOne({}, "-_id");
+    const { name } = await Author.findById(author);
+
+    res.status(200).json({ quote, day, name });
+  } catch (err) {
+    console.log(err);
+  }
 };
