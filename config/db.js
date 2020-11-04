7f305a9744fda0773c14e946feb3f44073c65305
jerjunkel <jerjunkel@gmail.com>
2020-11-04 10:24:06 -0500

    silence mongoose warnings

--- config/db.js
+++ config/db.js
@@ -4,6 +4,8 @@ const dbConnect = async () => {
   const dbConnInstance = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
   });
 
   console.log(`MongoDB Connected: ${dbConnInstance.connection.host}`);
