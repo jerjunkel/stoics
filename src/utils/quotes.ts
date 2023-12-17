24a6bb0647deadd4055f96b1447a138df067738b
Jermaine Kelly <jerjunkel@gmail.com>
2023-12-17 01:27:57 -0500

    refactor: use ternary operator

--- src/utils/quotes.ts
+++ src/utils/quotes.ts
@@ -1,11 +1,7 @@
 import Quote from "../models/Quote.js";
 const getRandomQuote = async (predicate = [{ $sample: { size: 1 } }]) => {
   const quotes = await Quote.aggregate(predicate);
-  if (quotes.length == 0) {
-    return null;
-  } else {
-    return quotes[0];
-  }
+  return quotes.length == 0 ? null : quotes[0];
 };
 
 export { getRandomQuote };
