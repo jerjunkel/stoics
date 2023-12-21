cb7decd5ed8c6b5826a1485d96148dc1601277fc
Jermaine Kelly <jerjunkel@gmail.com>
2023-12-21 17:55:45 -0500

    refactor: change to named export

--- src/controllers/quote.ts
+++ src/controllers/quote.ts
@@ -1,24 +1,24 @@
-import Express from "express";
+import { Response, Request } from "express";
 import Quote from "../models/Quote.js";
 import { getRandomQuote, getDailyQuote } from "../utils/quotes.js";
 
-const getAllQuotes = async (req: Express.Request, res: Express.Response) => {
+const getAllQuotes = async (req: Request, res: Response) => {
   const quotes = await Quote.find({});
   return res.send(quotes);
 };
 
-const getAQuote = async (req: Express.Request, res: Express.Response) => {
+const getAQuote = async (req: Request, res: Response) => {
   const id = req.params.id;
   const quote = await Quote.findById(id);
   return res.send(quote);
 };
 
-const getTodayQuote = async (req: Express.Request, res: Express.Response) => {
+const getTodayQuote = async (req: Request, res: Response) => {
   const quote = await getDailyQuote();
   return res.send(quote);
 };
 
-const getARandomQuote = async (req: Express.Request, res: Express.Response) => {
+const getARandomQuote = async (req: Request, res: Response) => {
   const quote = await getRandomQuote();
   res.send(quote);
 };
