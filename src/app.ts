7b09d9bf08a66cd8dd1e984e68ed76996f41425f
Jermaine Kelly <jerjunkel@gmail.com>
2024-01-30 19:46:50 -0500

    refactor: decouple databse and chron jobs from app

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,14 @@
 import express from "express";
 import routes from "./routes/index.js";
-import dailyQuoteJob from "./jobs/dailyqoute.js";
 import errorHandler from "./middlewares/errorHandler.js";
-import dbConnect from "./config/db.js";
 
 export default () => {
   const app = express();
 
-  // Connect to Database
-  dbConnect();
-
+  // Configure express
   app.use(express.json());
   app.use(routes);
   app.use(errorHandler);
-
-  // Cron job to update daily quote route
-  dailyQuoteJob.start();
 
   return app;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import makeApp from "./app.js";
 import config from "./config/index.js";
+import dbConnect from "./config/db.js";
+import dailyQuoteJob from "./jobs/dailyqoute.js";
 
 // initalize express
 const app = makeApp();
 
-app.listen(config.PORT, () => {
-  console.log(`Listening on port ${config.PORT}`);
-});
+try {
+  const db = await dbConnect();
+  dailyQuoteJob.start();
+  app.listen(config.PORT, () => {
+    console.log(`Listening on port ${config.PORT}`);
+  });
+} catch (error) {
+  console.log(error);
+}
