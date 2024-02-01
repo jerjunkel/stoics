c5b041962a9091fbc0c6f257c80482b7323e6025
Jermaine Kelly <jerjunkel@gmail.com>
2024-01-31 23:22:37 -0500

    test: calls quote repository once

--- tests/quote.service.spec.ts
+++ tests/quote.service.spec.ts
@@ -6,7 +6,13 @@ const spy = jest.spyOn(QuoteRespository, "readAll").mockResolvedValue([]);
 afterAll(() => {
   spy.mockReset();
 });
+
 describe("Quote Service tests", () => {
+  it("should be call repository once", async () => {
+    const quotes = await findAllQuotes();
+    expect(spy.mock.calls).toHaveLength(1);
+  });
+
   it("should return an array", async () => {
     const quotes = await findAllQuotes();
     expect(Array.isArray(quotes)).toBe(true);
