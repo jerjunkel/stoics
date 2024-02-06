58f8d1e2f75896af085d16259070d77497e00a0d
Jermaine Kelly <jerjunkel@gmail.com>
2024-02-06 02:24:01 -0500

    refactor: add strong type to find method

--- src/interfaces/Repository.ts
+++ src/interfaces/Repository.ts
@@ -2,5 +2,5 @@ export default interface IRepository<T> {
   add(element: T): void;
   remove(id: string): void;
   get(id: string): Promise<T | null>;
-  find(id: string): Promise<T[]>;
+  find(filter: {}): Promise<T[]>;
 }
--- src/repositories/quotes.repository.ts
+++ src/repositories/quotes.repository.ts
@@ -1,6 +1,6 @@
 import Quote from "../models/quotes.model.js";
 import { IRepository, IQuote } from "../interfaces/index.js";
-import mongoose from "mongoose";
+import { PipelineStage, FilterQuery, isValidObjectId } from "mongoose";
 
 export default class QuoteRespository implements IRepository<IQuote> {
   add(element: IQuote) {}
@@ -8,17 +8,17 @@ export default class QuoteRespository implements IRepository<IQuote> {
   remove(id: string) {}
 
   async get(id: string): Promise<IQuote | null> {
-    if (!mongoose.isValidObjectId(id)) return null;
+    if (!isValidObjectId(id)) return null;
     const quote = await Quote.findById(id).lean().exec();
     return quote;
   }
 
-  async find(filter: {}): Promise<IQuote[]> {
+  async find(filter: FilterQuery<IQuote>): Promise<IQuote[]> {
     const quotes = await Quote.find(filter).lean().populate("stoic", "name");
     return quotes;
   }
 
-  async aggregate(pipeline: mongoose.PipelineStage[]): Promise<IQuote[]> {
+  async aggregate(pipeline: PipelineStage[]): Promise<IQuote[]> {
     const quotes = await Quote.aggregate(pipeline);
     return quotes;
   }
