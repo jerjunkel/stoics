f6bd81e6e96126311ab8a8a891643d74908abc1c
Jermaine Kelly <jerjunkel@gmail.com>
2024-02-06 01:39:55 -0500

    test: returns a single quote by id

--- tests/quote.service.spec.ts
+++ tests/quote.service.spec.ts
@@ -1,10 +1,15 @@
 import QuoteService from "../src/services/quotes.service";
 import QuoteRespository from "../src/repositories/quotes.repository";
 import { IQuote } from "../src/interfaces";
+import { ErrorHandlingMiddlewareFunction } from "mongoose";
 
+interface MockResponseQuote extends IQuote {
+  id: string;
+}
 const mockRepo = new QuoteRespository();
 const sut = new QuoteService(mockRepo);
-const mockQuote: IQuote = {
+const mockQuote: MockResponseQuote = {
+  id: "123",
   text: "some inspirational quote",
   source: "n/a",
   tags: [{ name: "love", description: "" }],
@@ -17,12 +22,20 @@ const mockQuote: IQuote = {
 };
 const mockRepoFindSpy = jest.spyOn(mockRepo, "find").mockResolvedValue([]);
 const mockRepoAggregateSpy = jest.spyOn(mockRepo, "aggregate");
+const mockRepoGetOneSpy = jest.spyOn(mockRepo, "get");
 
 afterAll(() => {
   jest.clearAllMocks();
 });
 
 describe("Quote Service", () => {
+  describe("get a quote", () => {
+    it("should return a single quote if ID is found", async () => {
+      mockRepoGetOneSpy.mockResolvedValue(mockQuote);
+      const quote = (await sut.getAQuoteByID("123")) as MockResponseQuote;
+      expect(quote.id).toEqual(mockQuote.id);
+    });
+  });
   describe("get all quotes", () => {
     it("should be call repository once", async () => {
       await sut.getAllQuotes();
