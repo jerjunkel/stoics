9332fbabae5d3c42ce79d27bf1e1c6f80907f5f6
Jermaine Kelly <jerjunkel@gmail.com>
2024-02-06 01:44:06 -0500

    test returns null is no quote is found

--- tests/quote.service.spec.ts
+++ tests/quote.service.spec.ts
@@ -3,12 +3,12 @@ import QuoteRespository from "../src/repositories/quotes.repository";
 import { IQuote } from "../src/interfaces";
 import { ErrorHandlingMiddlewareFunction } from "mongoose";
 
-interface MockResponseQuote extends IQuote {
+interface IQuoteDTO extends IQuote {
   id: string;
 }
 const mockRepo = new QuoteRespository();
 const sut = new QuoteService(mockRepo);
-const mockQuote: MockResponseQuote = {
+const mockQuote: IQuoteDTO = {
   id: "123",
   text: "some inspirational quote",
   source: "n/a",
@@ -32,8 +32,14 @@ describe("Quote Service", () => {
   describe("get a quote", () => {
     it("should return a single quote if ID is found", async () => {
       mockRepoGetOneSpy.mockResolvedValue(mockQuote);
-      const quote = (await sut.getAQuoteByID("123")) as MockResponseQuote;
+      const quote = (await sut.getAQuoteByID("123")) as IQuoteDTO;
       expect(quote.id).toEqual(mockQuote.id);
+    });
+
+    it("should return null when no quote is found", async () => {
+      mockRepoGetOneSpy.mockResolvedValue(null);
+      const quote = await sut.getAQuoteByID("456");
+      expect(quote).toEqual(null);
     });
   });
   describe("get all quotes", () => {
