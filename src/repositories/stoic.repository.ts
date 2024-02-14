7bf8af0ac2c7d5342ea62f3a6a2e48489784f135
Jermaine Kelly <jerjunkel@gmail.com>
2024-02-13 19:53:50 -0500

    chore: remove console log

--- src/repositories/stoic.repository.ts
+++ src/repositories/stoic.repository.ts
@@ -20,7 +20,6 @@ export default class StoicRepository implements IRepository<IStoic> {
 
   async delete(id: string): Promise<Boolean> {
     const deleted = await Stoic.findByIdAndDelete(id);
-    console.log(deleted);
     return deleted ? true : false;
   }
 
